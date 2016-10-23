112c28136fb961eb60f1254efd4ae52c9a407cd2
Federico Knüssel <fknussel@gmail.com>
2016-10-23 18:14:17 +1100

    Bootstrap project

---
+++ README.md
@@ -0,0 +1,14 @@
+# baconify
+
+## Usage Example
+
+Have a look at this [example](https://github.com/fknussel/baconify-example).
+
+## Development Tasks
+
+| Command | Description |
+|---------|-------------|
+| `npm install` | Fetch dependencies and build binaries for any of the modules |
+| `npm run clean` | Remove `lib` directory |
+| `npm run build` | Build `lib/baconify.js` file |
+| `npm test` | Run test suite |
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: './src',
+  output: {
+    path: './lib',
+    filename: 'baconify.js',
+    library: 'baconify',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
